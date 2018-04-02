f075e28128d8ddfdc8f0a76107a878cbd58686cc
heybluguy <heybluguy@yahoo.com>
2018-04-02 03:37:45 -0400

    Ranges for first input to second input.

--- story.js
+++ story.js
@@ -1,3 +1,7 @@
-var input = prompt("Please type a number");
-var dieRoll = Math.floor(Math.random() * input) + 1;
-// alert(dieRoll);
+var input1 = prompt("Please type a starting number");
+var bottomNumber = parseInt(input1);
+var input = prompt("Please type an ending number");
+var topNumber = parseInt(input);
+var randomNumber = Math.floor(Math.random() * (topNumber - bottomNumber + 1)) + bottomNumber;
+var message = "<p>" + randomNumber + " is a number between " + bottomNumber + " and " + topNumber + ".</p>";
+document.write(message);
